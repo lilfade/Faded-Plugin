4cf58b19fd45c7b90c6501c43fbf5a431b13d1a8
Bryson Shepard <lilfade.lor@gmail.com>
2013-04-16 11:57:23 -0500

    Http working
    Added basic http get/post class to get registration started upon login,
    still working on that bit but that will be in php with jsonapi

--- src/me/lilfade/faded/Faded.java
+++ src/me/lilfade/faded/Faded.java
@@ -1,17 +1,15 @@
 package me.lilfade.faded;
 
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLConnection;
 import java.util.logging.Logger;
 
-import org.bukkit.ChatColor;
+//import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import me.lilfade.faded.Fadedhttp;
 
 public class Faded extends JavaPlugin{
 	public final Logger logger = Logger.getLogger("Minecraft");
@@ -36,22 +34,9 @@ public class Faded extends JavaPlugin{
 				sender.sendMessage("This command can only be used by players, not the console");
 			} else {
 				Player player = (Player) sender;
-				player.sendMessage(ChatColor.GOLD + "Sent");
-				player.sendMessage(ChatColor.AQUA + "Yourname: " + player.getName());
-				try {
-					URL fgURL = new URL("http://localhost/serverenable.php?cname=" + player.getName());
-					URLConnection fgURLConnection = fgURL.openConnection();
-					fgURLConnection.connect();
-				}
-				catch (MalformedURLException e) {
-					//new URL() failed
-					player.sendMessage(ChatColor.DARK_RED + "Url failed");
-				
-				}
-				catch (IOException e) {
-					// openCnnection() failed
-					player.sendMessage(ChatColor.DARK_RED + "open connection failed");
-				}
+				//player.sendMessage(ChatColor.GOLD + "Sent");
+				//send the character name to the web server to start the registration process
+				Fadedhttp.sendGetRequest("http://localhost/serverenable.php", "cname="+player.getName());
 			}
 			return true;
 		}
