6bef0169e9b2eec14640b345040e7f5d921f6481
Bryson Shepard <lilfade.lor@gmail.com>
2013-04-18 03:45:04 -0500

    Update v 0.03
    Added basic listener class to check for player first join, removed non
    needed code (most of it) re ordered some code ... working pretty good.

--- src/me/lilfade/faded/Faded.java
+++ src/me/lilfade/faded/Faded.java
@@ -1,46 +1,21 @@
 package me.lilfade.faded;
 
-import java.util.logging.Logger;
-
-//import org.bukkit.ChatColor;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
-import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import me.lilfade.faded.Fadedhttp;
-
 public class Faded extends JavaPlugin{
-	public final Logger logger = Logger.getLogger("Minecraft");
-	public static Faded plugin;
-	
-	@Override
-	public void onDisable() {
-		PluginDescriptionFile pdfFile = this.getDescription();
-		this.logger.info(pdfFile.getName() + " Has Been Disabled");
-	}
 	
 	@Override
 	public void onEnable() {
-		PluginDescriptionFile pdfFile = this.getDescription();
-		this.logger.info(pdfFile.getName() + " Version " + pdfFile.getVersion() + " Has been Enabled!");		
-		
+	//	PluginDescriptionFile pdfFile = this.getDescription();
+	//	this.logger.info(pdfFile.getName() + " Version " + pdfFile.getVersion() + " Has been Enabled!");		
+		PluginManager manager = this.getServer().getPluginManager();
+		manager.registerEvents(new FadedPlayerListener(),  this);
 	}
-	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
-		
-		if(cmd.getName().equalsIgnoreCase("sendme")){
-			if(!(sender instanceof Player)) {
-				sender.sendMessage("This command can only be used by players, not the console");
-			} else {
-				Player player = (Player) sender;
-				//player.sendMessage(ChatColor.GOLD + "Sent");
-				//send the character name to the web server to start the registration process
-				Fadedhttp.sendGetRequest("http://localhost/serverenable.php", "cname="+player.getName());
-			}
-			return true;
-		}
-		return false;
+	
+	@Override
+	public void onDisable() {
+	//	PluginDescriptionFile pdfFile = this.getDescription();
+	//	this.logger.info(pdfFile.getName() + " Has Been Disabled");
 	}
-
 }
