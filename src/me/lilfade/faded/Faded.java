1cff1713f87eb2958ec8b3e443e7ecb7661fa816
Bryson Shepard <lilfade.lor@gmail.com>
2013-04-20 03:30:46 -0500

    v 0.06
    Added some basic commands to send chat messages, will be changed to
    submit via http to website for database storage. TODO: remove hard coded
    url replace with var from config file, fix issues and suggestions to
    push data to website.

--- src/me/lilfade/faded/Faded.java
+++ src/me/lilfade/faded/Faded.java
@@ -1,10 +1,13 @@
 package me.lilfade.faded;
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Faded extends JavaPlugin{
-	
+
 	@Override
 	public void onEnable() {
 	//	PluginDescriptionFile pdfFile = this.getDescription();
@@ -17,5 +20,17 @@ public class Faded extends JavaPlugin{
 	public void onDisable() {
 	//	PluginDescriptionFile pdfFile = this.getDescription();
 	//	this.logger.info(pdfFile.getName() + " Has Been Disabled");
+	}
+	
+	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
+		Player player = (Player) sender;
+		if(commandLabel.equalsIgnoreCase("bug")) {
+			//we got a bug here
+			player.sendMessage("bug sent");
+		}else if(commandLabel.equalsIgnoreCase("suggestion")){
+			//we got a suggestion
+			player.sendMessage("suggestion sent");
+		}
+		return false;
 	}
 }
